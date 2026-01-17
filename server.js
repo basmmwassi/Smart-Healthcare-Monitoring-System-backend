0318c335596ae71a5bd4dadb598f5e71760d5f35
basm <basmmwassi100@gmail.com>
2026-01-17 22:58:57 +0200

    Add files via upload

--- server.js
+++ server.js
@@ -14,20 +14,17 @@ const allowedOrigins = new Set([
 
 app.use((req, res, next) => {
   const origin = req.headers.origin
+  res.setHeader('Vary', 'Origin')
 
-  if (origin && allowedOrigins.has(origin)) {
+  if (origin) {
     res.setHeader('Access-Control-Allow-Origin', origin)
-    res.setHeader('Vary', 'Origin')
   }
 
+  res.setHeader('Access-Control-Allow-Credentials', 'true')
   res.setHeader('Access-Control-Allow-Methods', 'GET,POST,PUT,DELETE,OPTIONS')
   res.setHeader('Access-Control-Allow-Headers', 'Content-Type,Authorization')
-  res.setHeader('Access-Control-Allow-Credentials', 'true')
 
-  if (req.method === 'OPTIONS') {
-    return res.sendStatus(204)
-  }
-
+  if (req.method === 'OPTIONS') return res.sendStatus(204)
   next()
 })
 

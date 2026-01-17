45dac5df54452db67c3c0b066698b921a5f08ee3
basm <basmmwassi100@gmail.com>
2026-01-17 18:21:58 +0200

    Add files via upload

--- Smart Healthcare Monitoring System backend/server.js
+++ Smart Healthcare Monitoring System backend/server.js
@@ -3,9 +3,30 @@ const express = require('express');
 const mongoose = require('mongoose');
 
 const app = express();
+const cors = require('cors');
 app.use(express.json());
 
 const PORT = process.env.PORT || 5000;
+
+const allowedOrigins = [
+  'https://smarthealthcare.netlify.app', 
+  'http://localhost:5500',
+  'http://127.0.0.1:5500'
+];
+
+app.use(cors({
+  origin: function (origin, callback) {
+  
+    if (!origin) return callback(null, true);
+
+    if (allowedOrigins.includes(origin)) return callback(null, true);
+
+    return callback(new Error('Not allowed by CORS: ' + origin));
+  },
+  methods: ['GET', 'POST', 'PUT', 'DELETE', 'OPTIONS'],
+  allowedHeaders: ['Content-Type', 'Authorization']
+}));
+
 
 app.listen(PORT, () => {
   console.log(`Server running on port ${PORT}`);
@@ -26,3 +47,5 @@ mongoose
 app.get('/', (req, res) => {
   res.send('Smart Healthcare Backend is running');
 });
+
+app.options('*', cors());
